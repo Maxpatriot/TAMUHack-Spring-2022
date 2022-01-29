b018be655fb5c22c562c6a927c564b77b37686a2
Maximilian <americana2013@gmail.com>
2022-01-29 17:06:28 -0600

    initial commit

---
+++ compulse/lib/getFile.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class getFileApp extends StatefulWidget {
+  const getFileApp({ Key? key }) : super(key: key);
+
+  @override
+  _getFileAppState createState() => _getFileAppState();
+}
+
+class _getFileAppState extends State<getFileApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
